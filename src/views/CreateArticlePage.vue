0ed138cca6a04ddf73185fed0b7307e8b2852b49
monoglo <monoglo37@gmail.com>
2020-10-19 17:50:13 +0800

    feat(CreateArticlePage): impl tags and text area

--- src/views/CreateArticlePage.vue
+++ src/views/CreateArticlePage.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row dense>
         <v-col cols="12">
-          <v-card outlined>
+          <v-card outlined elevation="18">
             <v-parallax
               height="200"
               src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
@@ -32,26 +32,93 @@
                   </v-list>
                 </v-menu>
               </v-app-bar>
-              <v-card-title class="white--text mt-16">
-                <p class="ml-3 font-weight-medium text-h4">滕王阁序</p>
+              <v-card-title class="white--text mt-12">
+                <v-text-field
+                  label="标题"
+                  dark
+                  class="ml-3 font-weight-medium text-h5"
+                ></v-text-field>
               </v-card-title>
             </v-parallax>
             <v-card-subtitle>
-              Listen to your favorite artists and albums whenever and wherever,
-              online and offline.
-            </v-card-subtitle>
-            <v-divider></v-divider>
-            <v-card-text>
-              <p class="text-body-1">
-                豫章故郡，洪都新府。星分翼轸，地接衡庐。襟三江而带五湖，控蛮荆而引瓯越。物华天宝，龙光射牛斗之墟；人杰地灵，徐孺下陈蕃之榻。雄州雾列，俊采星驰。台隍枕夷夏之交，宾主尽东南之美。都督阎公之雅望，棨戟遥临；宇文新州之懿范，襜帷暂驻。十旬休假，胜友如云；千里逢迎，高朋满座。腾蛟起凤，孟学士之词宗；紫电青霜，王将军之武库。家君作宰，路出名区；童子何知，躬逢胜饯。
-              </p>
-            </v-card-text>
-            <v-card-subtitle>
+              <v-chip class="ma-2" close @click:close="chip1 = false">
+                Closable
+              </v-chip>
+
+              <v-chip
+                class="ma-2"
+                close
+                color="red"
+                text-color="white"
+                @click:close="chip2 = false"
+              >
+                Remove
+              </v-chip>
+
+              <v-chip
+                class="ma-2"
+                close
+                color="green"
+                outlined
+                @click:close="chip3 = false"
+              >
+                Success
+              </v-chip>
+
+              <v-chip
+                class="ma-2"
+                close
+                color="orange"
+                label
+                outlined
+                @click:close="chip4 = false"
+              >
+                Complete
+              </v-chip>
               <v-chip class="ma-2" color="pink" label text-color="white">
                 <v-icon left> mdi-label </v-icon>
                 Tags
               </v-chip>
+              <v-menu bottom>
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn icon v-bind="attrs" v-on="on">
+                    <v-icon>mdi-tag-plus</v-icon>
+                  </v-btn>
+                </template>
+
+                <v-list>
+                  <template v-for="item in categories">
+                    <v-list-item
+                      v-if="!selected.includes(item)"
+                      :key="item.text"
+                      :disabled="loading"
+                      @click="selected.push(item)"
+                    >
+                      <v-list-item-avatar>
+                        <v-icon :disabled="loading" v-text="item.icon"></v-icon>
+                      </v-list-item-avatar>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item>
+                  </template>
+                </v-list>
+              </v-menu>
             </v-card-subtitle>
+            <v-divider></v-divider>
+            <v-card-text>
+              <v-textarea
+                outlined
+                name="input-7-4"
+                label="在此输入正文..."
+                class="ms-5 me-2"
+                height="280px"
+                no-resize
+              ></v-textarea>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" depressed @click="reserve" class="ms-7">
+                提交
+              </v-btn>
+            </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -60,7 +127,62 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      items: [
+        {
+          text: 'Nature',
+          icon: 'mdi-nature'
+        },
+        {
+          text: 'Nightlife',
+          icon: 'mdi-glass-wine'
+        },
+        {
+          text: 'November',
+          icon: 'mdi-calendar-range'
+        },
+        {
+          text: 'Portland',
+          icon: 'mdi-map-marker'
+        },
+        {
+          text: 'Biking',
+          icon: 'mdi-bike'
+        }
+      ],
+      loading: false,
+      search: '',
+      selected: []
+    }
+  },
+  computed: {
+    allSelected() {
+      return this.selected.length === this.items.length
+    },
+    categories() {
+      const search = this.search.toLowerCase()
+
+      if (!search) return this.items
+
+      return this.items.filter(item => {
+        const text = item.text.toLowerCase()
+
+        return text.indexOf(search) > -1
+      })
+    },
+    selections() {
+      const selections = []
+
+      for (const selection of this.selected) {
+        selections.push(selection)
+      }
+
+      return selections
+    }
+  }
+}
 </script>
 
 <style></style>
